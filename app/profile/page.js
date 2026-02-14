a927abea07bf33aafc7a176b809d91cf231f8de9
Rajathk16 <rajathkjogi01@gmail.com>
2026-02-14 23:14:17 +0530

    tailwind and animate

--- app/profile/page.js
+++ app/profile/page.js
@@ -27,15 +27,59 @@ export default function Profile() {
   }, []);
 
   return (
-    <div style={{ padding: "20px" }}>
-      <button onClick={() => router.back()}>⬅ Back</button>
+    <div className="min-h-screen  flex items-center justify-centerbg-gradient-to-br from-slate-800 via-slate-700 to-teal-600 px-4 text-white">
 
-      <h2>Profile</h2>
-      <p>Email: {userData?.email}</p>
-      <p>Level: {userData?.level}</p>
-      <p>Title: {userData?.title}</p>
-      <p>XP: {userData?.xp}</p>
-      <p>Quizzes Attempted: {quizCount}</p>
+      <div className="w-full max-w-md bg-white/10 backdrop-blur-xl rounded-3xl shadow-2xl p-8 border border-white/20
+      animate-fade-in">
+
+        <button
+          onClick={() => router.back()}
+          className="mb-5 px-4 py-2 rounded-xl bg-white/10 hover:bg-white/20 transition"
+        >
+          ⬅ Back
+        </button>
+
+        <div className="flex flex-col items-center">
+          <div className="w-24 h-24 rounded-full bg-gradient-to-r from-pink-500 to-indigo-500 flex items-center justify-center text-3xl font-bold shadow-lg animate-pulse">
+            {userData?.email?.charAt(0).toUpperCase()}
+          </div>
+
+          <h2 className="text-2xl font-bold mt-4">Profile</h2>
+          <p className="text-gray-300 text-sm">Gamified Dashboard</p>
+        </div>
+        <div className="mt-6 space-y-4">
+
+          <div className="flex justify-between bg-white/10 p-3 rounded-xl hover:bg-white/20 transition">
+            <span>📧 Email</span>
+            <span className="text-gray-200">{userData?.email}</span>
+          </div>
+
+          <div className="flex justify-between bg-white/10 p-3 rounded-xl hover:bg-white/20 transition">
+            <span>🧠 Level</span>
+            <span className="font-semibold">{userData?.level || 0}</span>
+          </div>
+
+          <div className="flex justify-between bg-white/10 p-3 rounded-xl hover:bg-white/20 transition">
+            <span>🏆 Title</span>
+            <span className="text-yellow-400 font-semibold">
+              {userData?.title || "Beginner"}
+            </span>
+          </div>
+
+          <div className="flex justify-between bg-white/10 p-3 rounded-xl hover:bg-white/20 transition">
+            <span>⚡ XP</span>
+            <span className="text-green-400 font-bold">
+              {userData?.xp || 0}
+            </span>
+          </div>
+
+          <div className="flex justify-between bg-white/10 p-3 rounded-xl hover:bg-white/20 transition">
+            <span>📝 Quizzes</span>
+            <span>{quizCount}</span>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
